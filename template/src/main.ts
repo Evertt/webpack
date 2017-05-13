03d8d932edda308329e4ce28a789357f29ce9abe
Evert van Brussel <ecvanbrussel@gmail.com>
2017-05-13 05:56:42 +0200

    meta typo

--- template/src/main.ts
+++ template/src/main.ts
@@ -15,7 +15,7 @@ Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
-  {{#router}}router, {{/router}}{{#vuex}}store, {{#vuex}}
+  {{#router}}router, {{/router}}{{#vuex}}store, {{/vuex}}
   {{#if_eq build "runtime"}}
   render: h => h(App)
   {{/if_eq}}
